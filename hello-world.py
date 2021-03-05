a1af93598f8991bfb825a48865789548afa83665
Kajal Chatterjee <68781358+kajal-0101@users.noreply.github.com>
2021-03-06 00:21:32 +0530

    initial commit in the main branch

---
+++ hello-world.py
@@ -0,0 +1,10 @@
+'''
+This is a basic python program. The program is being created to make it a part of various git commands' demonstrations.
+'''
+
+
+'''
+This is the first commit. 
+A line "Hello World is going to be printed"
+'''
+print("Hello World")
